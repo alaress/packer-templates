a0c6f3c2f9078a2850669d5ab142954ab7ec11a3
Sam Bailey <cyprix@cyprix.com.au>
2013-12-21 09:57:08 +1100

    initial ubuntu 14.04 trusty alpha

---
+++ ubuntu-1404/scripts/kernelupgrade.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Update the linux kernel
+apt-get -y update
+#apt-get -y install linux-generic-lts-quantal linux-image-generic-lts-quantal
+apt-get -y autoremove
+apt-get clean
+
+reboot
+sleep 60
+exit
