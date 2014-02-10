8adbbb4a72c6f748105bff91dfc456dc4b084841
Sam Bailey <sam.bailey@alaress.com.au>
2014-02-10 11:49:15 +1100

    update to latest ubuntu 14.04.4 release

--- ubuntu-1204-gold/scripts/kernelupgrade.sh
+++ ubuntu-1204-gold/scripts/kernelupgrade.sh
@@ -2,7 +2,7 @@
 
 # Update the linux kernel
 apt-get -y update
-apt-get -y install linux-generic-lts-raring linux-image-generic-lts-raring
+apt-get -y install linux-generic-lts-saucy linux-image-generic-lts-saucy
 apt-get -y autoremove
 apt-get clean
 
