f9161cd566b4ea5a84b8901d299f7f21ff98fc4d
Eli Lowinger <low613@gmail.com>
2022-09-21 12:12:06 +1000

    set appriate upstream for bionic [IST-1671]

--- ubuntu-1804-php74/scripts/vagrant.sh
+++ ubuntu-1804-php74/scripts/vagrant.sh
@@ -5,7 +5,7 @@ date '+%s' >> /home/vagrant/.vagrant.buildtimestamp
 
 # Download and Install Schoolbox's Repo
 echo '# schoolbox-upstream-new
-deb https://apt.schoolbox.com.au/upstream bionic main' > /etc/apt/sources.list.d/schoolbox-upstream-new.list
+deb https://apt.schoolbox.com.au/upstream bionic bionic' > /etc/apt/sources.list.d/schoolbox-upstream-new.list
 wget -qO - http://apt.schoolbox.com.au/schoolbox.key | apt-key add -
 apt-get update
 
